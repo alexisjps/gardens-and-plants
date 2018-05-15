dfd72bd1f5821c1dc7db1141e5dbfc3ef68784d0
Edward Schults <edward.schults@gmail.com>
2018-05-15 14:56:35 +0200

    frontend setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
 //= require_tree .
