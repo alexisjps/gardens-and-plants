44eab1a9f70d1a271f3cdbe326dba6cbb41cd511
Edward Schults <edward.schults@gmail.com>
2018-05-15 17:27:17 +0200

    add container and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :gardens
+  resources :gardens do
+    resources :plants, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
