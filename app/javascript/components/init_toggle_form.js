fb03c0d9be807cd8c878f9f927764b4ca38e348e
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-10-22 12:09:30 +0200

    update toggle form button

--- app/javascript/components/init_toggle_form.js
+++ app/javascript/components/init_toggle_form.js
@@ -1,13 +1,10 @@
 const initToggleForm = () => {
   const button = document.querySelector('.toggle-form');
   const form = document.querySelector('#new_plant');
-  if (button) {
-    button.addEventListener('click', () => {
-      form.classList.toggle('d-none');
-    });
-  }
-  console.log(button)
-  console.log(form)
+  button.addEventListener('click', () => {
+    form.classList.remove('d-none');
+    button.classList.add('d-none');
+  });
 }
 
 export { initToggleForm };
