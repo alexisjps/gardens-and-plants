35af6445e9d5dc8344b965e4365419d76fb7c5e7
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-06-30 18:16:19 +0200

    remove unused files

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,5 @@
 class Plant < ApplicationRecord
   belongs_to :garden
-  has_many :plant_tags, dependent: :destroy
-  has_many :tags, through: :plant_tags
 
   has_many :plant_tags, dependent: :destroy
   has_many :tags, through: :plant_tags
